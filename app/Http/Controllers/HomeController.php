35b8459c1dd69ba9c962be99889c6a7c90affef6
SokunSovisal <sovisalsokun@gmail.com>
2021-04-10 20:08:08 +0700

    update upload button

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Spatie\Permission\Models\Role;
 use Spatie\Permission\Models\Permission;
+use Illuminate\Support\Facades\Storage;
 use App\Models\User;
 use App\Models\Patient;
 use App\Models\Doctor;
